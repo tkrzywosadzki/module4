8f4c7f4940417339b206759928dbcbaae74ded13
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-01-18 23:19:50 +0100

    add playerMove to script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Moj ruch to ' + playerMove);
